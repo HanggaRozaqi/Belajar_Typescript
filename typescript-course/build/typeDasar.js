3fd688eee24c4372b9387c701fe8c3852c39b14e
hangga_yuda <hanggayudarozaqi@gmail.com>
2021-02-07 13:43:38 +0700

    modify course variable string

---
+++ typescript-course/build/typeDasar.js
@@ -0,0 +1,8 @@
+"use strict";
+// Deklarasi variabel string pada typecsript #1
+var $nama;
+// Deklarasi variabel string pada typecsript #2
+var $name = "Garaga";
+$name = "Boruto";
+$nama = "90";
+console.log($name + " " + $nama);
---
+++ typescript-course/src/typeDasar.ts
@@ -0,0 +1,8 @@
+// Deklarasi variabel string pada typecsript #1
+let $nama: string;
+ 
+// Deklarasi variabel string pada typecsript #2
+let $name = "Garaga";
+$name = "Boruto";
+$nama = "90";
+console.log($name+" "+$nama)
