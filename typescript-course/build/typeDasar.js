3f3dcf7cc0b1209c6d7e5433498ef6ab193b6da2
hangga_yuda <hanggayudarozaqi@gmail.com>
2021-02-07 22:39:11 +0700

    course integer and boolean variable

--- typescript-course/build/typeDasar.js
+++ typescript-course/build/typeDasar.js
@@ -6,3 +6,10 @@ var $name = "Garaga";
 $name = "Boruto";
 $nama = "90";
 console.log($name + " " + $nama);
+// Deklarasi Variabel Number
+var $number;
+$number = 100;
+// Deklarasi Variabel Boolean 
+var $boolean;
+$boolean = true;
+console.log("ini adalah number " + $number + " dan " + $boolean);
--- typescript-course/src/typeDasar.ts
+++ typescript-course/src/typeDasar.ts
@@ -6,3 +6,13 @@ let $name = "Garaga";
 $name = "Boruto";
 $nama = "90";
 console.log($name+" "+$nama)
+
+// Deklarasi Variabel Number
+let $number: number;
+$number = 100;
+
+// Deklarasi Variabel Boolean 
+let $boolean: boolean;
+$boolean = true;
+
+console.log("ini adalah number "+$number+" dan "+$boolean);
